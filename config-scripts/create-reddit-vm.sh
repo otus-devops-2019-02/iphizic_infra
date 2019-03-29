9daed510f96d8eb82bca8a736a6132ac4371c7d3
Dmitriy Erohin <dmitriy.erohin@tularegion.ru>
2019-03-29 15:38:39 +0300

    Release

--- config-scripts/create-reddit-vm.sh
+++ config-scripts/create-reddit-vm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 gcloud compute instances create reddit-immutable \
            --boot-disk-size=10GB \
-           --image-family reddit-base-immutable \
+           --image-family reddit-full \
            --machine-type=g1-small \
            --tags puma-server \
            --restart-on-failure
