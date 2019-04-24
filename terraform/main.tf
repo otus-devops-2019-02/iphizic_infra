e655aaf3f851782549e4e47f7a496de52c74c05c
Dmitriy Erohin <dmitriy.erohin@tularegion.ru>
2019-04-24 22:03:47 +0300

    Before two resorses

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,8 +37,12 @@ resource "google_compute_instance" "app" {
     network = "default"
 
     # использовать ephemeral IP для доступа из Интернет
-    access_config {}
+    access_config = {
+nat_ip = "${google_compute_address.app_ip.address}"
+}
   }
+
+
 
   connection {
     type  = "ssh"
@@ -77,3 +81,19 @@ resource "google_compute_firewall" "firewall_puma" {
   # Правило применимо для инстансов с перечисленными тэгами
   target_tags = ["reddit-app"]
 }
+
+resource "google_compute_firewall" "firewall_ssh" {
+name = "default-allow-ssh"
+network = "default"
+allow {
+protocol = "tcp"
+ports = ["22"]
+}
+source_ranges = ["0.0.0.0/0"]
+}
+resource "google_compute_address" "app_ip" {
+name = "reddit-app-ip"
+
+}
+
+
