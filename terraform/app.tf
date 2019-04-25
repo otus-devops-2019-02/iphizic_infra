98b45b4bb434be8b9f8858a7288f4d40bc8730dd
Dmitriy Erohin <dmitriy.erohin@tularegion.ru>
2019-04-25 20:35:35 +0300

    Pre modules

---
+++ terraform/app.tf
@@ -0,0 +1,55 @@
+resource "google_compute_instance" "app" {
+  count        = 1
+  name         = "reddit-app-${count.index}"
+  machine_type = "g1-small"
+  zone         = "${var.app_zone}"
+  tags         = ["reddit-app"]
+
+  metadata {
+    # путь до публичного ключа
+    ssh-keys = "iphizic:${file(var.public_key_path)}\nappuser:${file(var.public_key_path)}\nappuser2:${file(var.public_key_path)}"
+  }
+
+  # определение загрузочного диска
+  boot_disk {
+    initialize_params {
+      image = "${var.app_disk_image}"
+    }
+  }
+
+  # определение сетевого интерфейса
+  network_interface {
+    # сеть, к которой присоединить данный интерфейс
+    network = "default"
+
+    # использовать ephemeral IP для доступа из Интернет
+    access_config = {
+nat_ip = "${google_compute_address.app_ip.address}"
+}
+  }
+
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+  name = "allow-puma-default"
+
+  # Название сети, в которой действует правило
+  network = "default"
+
+  # Какой доступ разрешить
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+
+  # Каким адресам разрешаем доступ
+  source_ranges = ["0.0.0.0/0"]
+
+  # Правило применимо для инстансов с перечисленными тэгами
+  target_tags = ["reddit-app"]
+}
+
+resource "google_compute_address" "app_ip" {
+name = "reddit-app-ip"
+
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,8 +14,13 @@ variable public_key_path {
   description = "Path to the public key used for ssh access"
 }
 
-variable disk_image {
+variable app_disk_image {
   description = "Disk image"
+}
+
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default = "reddit-db-base"
 }
 
 variable private_key_path {
