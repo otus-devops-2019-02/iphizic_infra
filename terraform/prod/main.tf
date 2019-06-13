bffb25f1f923fa1a6c101309d5f396cac476cbe6
Dmitriy Erohin <dmitriy.erohin@tularegion.ru>
2019-06-13 10:11:34 +0300

    Fix modules path

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -13,14 +13,14 @@ provider "google" {
 }
 
 module "app" {
-  source          = "modules/app"
+  source          = "../modules/app"
   public_key_path = "${var.public_key_path}"
   app_zone        = "${var.app_zone}"
   app_disk_image  = "${var.app_disk_image}"
 }
 
 module "db" {
-  source          = "modules/db"
+  source          = "../modules/db"
   public_key_path = "${var.public_key_path}"
   app_zone        = "${var.app_zone}"
   db_disk_image   = "${var.db_disk_image}"
