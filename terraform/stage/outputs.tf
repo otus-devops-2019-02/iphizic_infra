c7c6adb15051fbe8c7897587337035ed51a1ac14
iphizic <iphizic@gmail.com>
2019-06-27 00:18:18 +0300

    Ansible 3 (#9)
    * First stage
    
    * To stage 3
    
    * done stage 3
    
    * Star commit
    
    * Travis look
    
    * Travis look debug
    
    * Travis look to be done
    
    * Add info to readme
    
    * First stage
    
    * Stage all
    
    * Add comunity role
    
    * Add unstar done
    
    * Debug unstar done
    
    * Debug unstar new string in users
    
    * unstar done
    
    * add secret vars
    
    * trytravis
    
    * trytravis2
    
    * trytravis3
    
    * trytravis final
    
    * final travis
    
    * lol kek
    
    * lol kek
    
    * lol kek tets done
    
    * lol kek maybe done
    
    * fix provisioner packer
    
    * Add fix example
    
    * fix terraform
    
    * Fix exampl

--- terraform/stage/outputs.tf
+++ terraform/stage/outputs.tf
@@ -2,6 +2,11 @@
 //  value = "${google_compute_forwarding_rule.app_frontend.ip_address}"
 //}
 
+
 output "app_external_ip" {
-  value = "${module.app.app_external_ip}"
+  value = "${module.app.external_ip}"
+}
+
+output "db_external_ip" {
+  value = "${module.db.external_ip}"
 }
